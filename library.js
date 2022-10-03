25c4dc9d83e5ee20ab25cb43148a4789fbbdcc58
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-10-02 23:51:19 -0400

    New books are added to book array and displayed in table

--- library.js
+++ library.js
@@ -15,13 +15,6 @@ const book2 = new Book('Survivor', 'Again Chuck');
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
-const bookForm = document.getElementById('books');
-const title = bookForm.elements['bookTitle'];
-const author = bookForm.elements['bookAuthor'];
-
-let bookTitle = title.value;
-let bookAuthor = author.value;
-
 document.addEventListener('submit', function(e){
   e.preventDefault()
   const bookForm = document.querySelector("#books");
@@ -32,5 +25,30 @@ document.addEventListener('submit', function(e){
   const bookObj = new Book(bookTitle, bookAuthor)
   addBookToLibrary(bookObj)
   bookForm.reset()
+  addToTable()
 })
+
+
+function addToTable() {
+  let table = document.getElementById("libraryTable");
+  let row = table.insertRow();
+  let numberRow = row.insertCell();
+  let titleCell = row.insertCell();
+  let authorCell = row.insertCell();
+  numberRow.innerHTML = myLibrary.length;
+  titleCell.innerHTML = myLibrary[myLibrary.length - 1].title;
+  authorCell.innerHTML = myLibrary[myLibrary.length - 1].author;
+}
+
+
+for (let i =0; i < myLibrary.length; i++) {
+  let table = document.getElementById("libraryTable");
+  let row = table.insertRow();
+  let numberRow = row.insertCell();
+  let titleCell = row.insertCell();
+  let authorCell = row.insertCell();
+  numberRow.innerHTML = (i+1);
+  titleCell.innerHTML = myLibrary[i].title;
+  authorCell.innerHTML = myLibrary[i].author;
+}
 
