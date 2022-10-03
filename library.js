b9a8611d9101508d46f728df5b9bd15d99441ce1
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-10-02 22:38:41 -0400

    Connect JS file and event listner

--- library.js
+++ library.js
@@ -14,3 +14,29 @@ const book2 = new Book('Survivor', 'Again Chuck');
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+
+function bookSubmit(event) {
+  let newBook = new Book(bookTitle, bookAuhtor);
+  addBookToLibrary(newBook);
+  log.textContent = 'form submitted';
+}
+
+const bookForm = document.getElementById('books');
+const title = bookForm.elements['bookTitle'];
+const author = bookForm.elements['bookAuthor'];
+
+let bookTitle = title.value;
+let bookAuthor = author.value;
+
+document.addEventListener('submit', function(e){
+  e.preventDefault()
+  const bookForm = document.getElementById('books');
+  const title = bookForm.elements['bookTitle']
+  const author = bookForm.elements['bookAuthor']
+  let bookTitle = title.value
+  let bookAuthor = author.value
+  const bookObj = new Book(title, author)
+  addBookToLibrary(bookObj)
+
+})
+
