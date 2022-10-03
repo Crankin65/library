8a38af514107cb08383716b0f61ba1dcf0941f9d
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-10-02 22:55:34 -0400

    Submitted titles/authors will add to library array

--- library.js
+++ library.js
@@ -15,12 +15,6 @@ const book2 = new Book('Survivor', 'Again Chuck');
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
-function bookSubmit(event) {
-  let newBook = new Book(bookTitle, bookAuhtor);
-  addBookToLibrary(newBook);
-  log.textContent = 'form submitted';
-}
-
 const bookForm = document.getElementById('books');
 const title = bookForm.elements['bookTitle'];
 const author = bookForm.elements['bookAuthor'];
@@ -30,13 +24,13 @@ let bookAuthor = author.value;
 
 document.addEventListener('submit', function(e){
   e.preventDefault()
-  const bookForm = document.getElementById('books');
+  const bookForm = document.querySelector("#books");
   const title = bookForm.elements['bookTitle']
   const author = bookForm.elements['bookAuthor']
   let bookTitle = title.value
   let bookAuthor = author.value
-  const bookObj = new Book(title, author)
+  const bookObj = new Book(bookTitle, bookAuthor)
   addBookToLibrary(bookObj)
-
+  bookForm.reset()
 })
 
