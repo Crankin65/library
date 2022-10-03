47d7ca7c41363a1ef942d4258bb3381bb55ba3bf
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-10-03 01:52:42 -0400

    Numbers in each row are indicative of the index of the bok in the array (duplicate titles will cause bugs)

--- library.js
+++ library.js
@@ -11,11 +11,11 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-const book1 = new Book('Haunted', 'Chuck', 200, 'Read');
-const book2 = new Book('Survivor', 'Again Chuck', 150, 'Not Read');
+// const book1 = new Book('Haunted', 'Chuck', 200, 'Read');
+// const book2 = new Book('Survivor', 'Again Chuck', 150, 'Not Read');
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
 
 document.addEventListener('submit', function(e){
   e.preventDefault()
@@ -30,12 +30,14 @@ document.addEventListener('submit', function(e){
   let readStatus = read.value
   const bookObj = new Book(bookTitle, bookAuthor, numberOfPages, readStatus)
   addBookToLibrary(bookObj)
-  addToTable()
+  const newBookIndex = myLibrary.map(object => object.title).indexOf(bookTitle)
+  addToTable(newBookIndex)
   bookForm.reset()
+  console.log(newBookIndex)
 })
 
 
-function addToTable() {
+function addToTable(index) {
   let table = document.getElementById("libraryTable");
   let row = table.insertRow();
   let numberRow = row.insertCell();
@@ -43,26 +45,54 @@ function addToTable() {
   let authorCell = row.insertCell();
   let pageCell = row.insertCell();
   let readCell = row.insertCell();
-  numberRow.innerHTML = myLibrary.length;
-  titleCell.innerHTML = myLibrary[myLibrary.length - 1].title;
-  authorCell.innerHTML = myLibrary[myLibrary.length - 1].author;
-  pageCell.innerHTML = myLibrary[myLibrary.length - 1].numberOfPages;
-  readCell.innerHTML = myLibrary[myLibrary.length -1].readStatus;
+  let deleteCell = row.insertCell();
+  numberRow.innerHTML = index+1;
+  titleCell.innerHTML = myLibrary[index].title;
+  authorCell.innerHTML = myLibrary[index].author;
+  pageCell.innerHTML = myLibrary[index].numberOfPages;
+  readCell.innerHTML = myLibrary[index].readStatus;
+
+  let deletebtn = document.createElement('button');
+  deletebtn.setAttribute("bookNumber", index + 1);
+  deletebtn.innerText = 'Delete';
+  deletebtn.onclick = function() {
+    delete myLibrary[deletebtn.getAttribute("bookNumber")];
+    document.getElementById("libraryTable").deleteRow(deletebtn.getAttribute("bookNumber"))
+    
+  }
+
+  deleteCell.appendChild(deletebtn);
 }
 
+// for (let i =0; i < myLibrary.length; i++) {
+//   let table = document.getElementById("libraryTable");
+//   let row = table.insertRow();
+//   let numberRow = row.insertCell();
+//   let titleCell = row.insertCell();
+//   let authorCell = row.insertCell();
+//   let pageCell = row.insertCell();
+//   let readCell = row.insertCell();
+//   let deleteCell = row.insertCell();
+//   numberRow.innerHTML = (i+1);
+//   titleCell.innerHTML = myLibrary[i].title;
+//   authorCell.innerHTML = myLibrary[i].author;
+//   pageCell.innerHTML = myLibrary[i].numberOfPages;
+//   readCell.innerHTML = myLibrary[i].readStatus;
+  
+//   let deletebtn = document.createElement('button');
+//   deletebtn.setAttribute("bookNumber", i + 1);
+//   deletebtn.innerText = 'Delete';
+//   deletebtn.onclick = function() {
+//     delete myLibrary[deletebtn.getAttribute("bookNumber")];
+//     document.getElementById("libraryTable").deleteRow(deletebtn.getAttribute("bookNumber"))
+//   }
 
-for (let i =0; i < myLibrary.length; i++) {
-  let table = document.getElementById("libraryTable");
-  let row = table.insertRow();
-  let numberRow = row.insertCell();
-  let titleCell = row.insertCell();
-  let authorCell = row.insertCell();
-  let pageCell = row.insertCell();
-  let readCell = row.insertCell();
-  numberRow.innerHTML = (i+1);
-  titleCell.innerHTML = myLibrary[i].title;
-  authorCell.innerHTML = myLibrary[i].author;
-  pageCell.innerHTML = myLibrary[i].numberOfPages;
-  readCell.innerHTML = myLibrary[i].readStatus;
-}
+//   deleteCell.appendChild(deletebtn);
+  
+  
+// }
+
+// To do
+// Update number column to dynamically update in column (Use findIndex based on title during delete button)
+// Make delete row function to look at index based on number in row
 
